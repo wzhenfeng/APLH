ef1291ddc35ce20f931a694113c385730d23455c
wzhenfeng <wongzhenfeng05@gmail.com>
2026-05-18 17:06:53 +0800

    Adding Learning Materials Function

--- Pages/Learning.cshtml.cs
+++ Pages/Learning.cshtml.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using APLH.Models;
+using APLH.Services;
 
-namespace MyApp.Namespace
+namespace APLH.Pages
 {
     public class LearningModel : PageModel
     {
-        public void OnGet()
+        private readonly LearningService _service;
+
+        public LearningModel(LearningService service)
         {
+            _service = service;
+        }
+
+        public Course Course { get; set; } = new();
+
+        public List<CourseMaterial> Materials { get; set; } = new();
+
+        public async Task<IActionResult> OnGetAsync(int courseId)
+        {
+            Course = await _service.GetCourseByIdAsync(courseId);
+
+            if (Course == null)
+            {
+                return RedirectToPage("/Courses");
+            }
+
+            Materials = (await _service.GetCourseMaterialsAsync(courseId)).ToList();
+
+            return Page();
         }
     }
 }
