c32444445405c4069d51699871e2eff4a92d2602
wzhenfeng <wongzhenfeng05@gmail.com>
2026-07-16 18:02:20 +0800

    Admin dashboard course management add button

--- obj/Debug/net8.0/APLH.AssemblyInfo.cs
+++ obj/Debug/net8.0/APLH.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APLH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e4b2389a69dc2c14549a7ced76d9a6abc7069d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79210dcb0d7056a9bd3978b1c990d8c5986a8627")]
 [assembly: System.Reflection.AssemblyProductAttribute("APLH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APLH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
