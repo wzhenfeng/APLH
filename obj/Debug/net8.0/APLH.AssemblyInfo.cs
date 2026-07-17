6e163eee5ef3c686b6a4bdb8022a4055afc31383
wzhenfeng <wongzhenfeng05@gmail.com>
2026-07-17 21:18:44 +0800

    Update

--- obj/Debug/net8.0/APLH.AssemblyInfo.cs
+++ obj/Debug/net8.0/APLH.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APLH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93bd46c7b42f9ba499e36263db6f400b9e010be5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af96fddf58fce0ac9e7ff257ca63f8222e19e3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("APLH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APLH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
